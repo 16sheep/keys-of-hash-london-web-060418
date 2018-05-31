47c49e2b34e10031a99262841c613ee88e479943
marjutubli@gmail.com <marjutubli@gmail.com>
2018-05-31 09:58:02 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -9,14 +9,3 @@ class Hash
   arr
 end
 
-class Hash
-  def keys_of(*arguments)
-    matches = []
-    self.each do |key, value|
-      if arguments.include?(value)
-        matches << key
-      end
-    end
-    return matches
-  end
-end
